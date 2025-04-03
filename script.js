8d316813e004740c030589f99e15635da06de4b6
AureaOrganization <ayakinatsumo@gmail.com>
2025-04-02 23:37:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,3 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.style.opacity = 1;
     }, 500);
 });
-
-document.addEventListener("keydown", function(event) {
-    if (event.key === "c") {  
-        let hiddenPosts = document.querySelectorAll(".hidden-post");
-        hiddenPosts.forEach(post => {
-            post.style.display = "block";
-        });
-    }
-});
